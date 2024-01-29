8a4d4ad2f35148e936ac1dbd1fdb41987bba1c75
SEEKFREE_BUDING <2289331269@qq.com>
2024-01-29 18:12:44 +0800

    V3.0.8         修正ADC枚举长度         优化串口通信效率

--- Example/E02_uart_demo/user/main_cm7_0.c
+++ Example/E02_uart_demo/user/main_cm7_0.c
@@ -72,8 +72,8 @@
 // ******************************* 代码区域 *******************************
 #define UART_INDEX              (DEBUG_UART_INDEX   )                           // 默认 UART_0
 #define UART_BAUDRATE           (DEBUG_UART_BAUDRATE)                           // 默认 115200
-#define UART_TX_PIN             (DEBUG_UART_TX_PIN  )                           // 默认 UART0_TX_P14_0
-#define UART_RX_PIN             (DEBUG_UART_RX_PIN  )                           // 默认 UART0_RX_P14_1
+#define UART_TX_PIN             (DEBUG_UART_TX_PIN  )                           // 默认 UART0_TX_P00_1
+#define UART_RX_PIN             (DEBUG_UART_RX_PIN  )                           // 默认 UART0_RX_P00_0
 
 uint8 uart_get_data[64];                                                        // 串口接收数据缓冲区
 uint8 fifo_get_data[64];                                                        // fifo 输出读出缓冲区
